15a6ab95f556163c468523f4f2f6f6f7e228dc74
Alan Liu <alanl.liu@yahoo.com>
2016-04-02 12:11:01 -0400

    added default expandable list view with message placeholders

--- app/src/main/java/com/bottle/alan/messageinabottle/ScrollingMessages.java
+++ app/src/main/java/com/bottle/alan/messageinabottle/ScrollingMessages.java
@@ -8,25 +8,164 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
 
 public class ScrollingMessages extends AppCompatActivity {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling_messages);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_scrolling_messages); // REPLACE WITH ACTIVITY_MAIN
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        // RANDOM TOOLBAR
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        // OK!
+        // NOW I use android stuff
+        // get the listview
+
+
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+////         preparing list data, POPULATE OUR MESSAGES
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapters(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
+
     }
+
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        // GET INFO FROM ALL THE MESSAGES
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Received Messages");
+        listDataHeader.add("Sent Messages");
+        listDataHeader.add("Archived Messages");
+
+        // Adding child data
+        List<String> received = new ArrayList<String>();
+        received.add("HackPton is the best!!!");
+        received.add("I have two midterms on Monday T.T");
+        received.add("Wow the applications are endless");
+        received.add("Where are the bathrooms around here???");
+//        received.add("filler");
+//        received.add("The Dark Knight");
+//        received.add("12 Angry Men");
+
+        List<String> sent = new ArrayList<String>();
+        sent.add("Blah blah blah");
+        sent.add("blah blah");
+        sent.add("blah");
+//        sent.add("Grown Ups 2");
+//        sent.add("Red 2");
+//        sent.add("The Wolverine");
+
+        List<String> archived = new ArrayList<String>();
+        archived.add("deleted 1");
+        archived.add("deleted 2");
+        archived.add("deleted 3");
+//        archived.add("The Canyons");
+//        archived.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), received); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), sent);
+        listDataChild.put(listDataHeader.get(2), archived);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
